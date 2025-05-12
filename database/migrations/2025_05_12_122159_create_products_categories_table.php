be3b940e254b8402ef929225152f06610725a0a3
richxrd7 <richardquintanapadron@gmail.com>
2025-05-12 13:27:02 +0100

    adjustment on the migration

--- database/migrations/2025_05_12_122159_create_products_categories_table.php
+++ database/migrations/2025_05_12_122159_create_products_categories_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Product;
 use App\Models\Category;
 
 return new class extends Migration
@@ -13,14 +14,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('products_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('images');
-            $table->string('name');
-            $table->string('description');
-            $table->decimal('price', 7, 2);
-            $table->boolean('is_discounted')->default(false);
-            $table->decimal('discount', 5, 2)->default(0.0);
+            $table->foreignIdFor(Product::class);
+            $table->foreignIdFor(Category::class);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('products_categories');
     }
 };
